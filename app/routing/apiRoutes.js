11423c81d216d35239b8b1a1e5c38b89de70dbbf
Cloud <claudiojsaillant@gmail.com>
2019-08-23 11:38:16 -0400

    fixing routes

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -1,24 +1,61 @@
-var express = require('express');
 var friendsFile = require('../data/friends');
 var data = friendsFile.friends;
-console.log('connected')
 
-var app = express();
-var PORT = process.env.PORT || 3000;
+var friendSums = [];
+var arr1 = [5, 1, 4, 4, 5, 1, 2, 5, 4, 1];
+var objArray = [
+  [3, 2, 6, 4, 5, 1, 2, 5, 4, 1],
+  [5, 1, 4, 4, 5, 1, 2, 5, 4, 1],
+]
 
-// Sets up the Express app to handle data parsing
-app.use(express.urlencoded({ extended: true }));
-app.use(express.json());
+function totalDifference(arr1, arr2) {
+  sum = 0;
+  for (i = 0; i < arr1.length; i++) {
+    add = (arr1[i] - arr2[i]);
+    if (add < 0) {
+      add = add * -1;
+      sum = sum + add;
+    }
+    else {
+      sum = sum + add;
+    }
+  }
+  friendSums.push(sum);
+}
 
-app.get("/api/friends", function (req, res) {
+function objectLoop(arre, myArray) {
+  for (j = 0; j < arre.length; j++) {
+
+    totalDifference(arre[j], myArray)
+
+  }
+  bestIndex = friendSums.indexOf(Math.min.apply(null, arr));
+  friendSums = [];
+  return bestIndex;
+}
+
+
+console.log(objectLoop(objArray, arr1));
+
+
+module.exports = function (app) {
+  app.get("/api/friends", function (req, res) {
+    data.push('hello');
     res.json(data);
-});
-
-app.post("/api/friends", function(req, res) {
-    
-    
   });
 
-  console.log('end')
+  app.post("/api/friends", function (req, res) {
+    jsonData = req.body;
+
+    data.push(jsonData);
+  });
+
+}
+
+
+
+
+
+
 
 
--- app/routing/htmlRoutes.js
+++ app/routing/htmlRoutes.js
@@ -1,23 +1,12 @@
-var express = require('express');
 var path = require('path');
-var apiRout = require('../routing/apiRoutes');
 
-var app = express();
-var PORT = process.env.PORT || 3000;
+module.exports = function (app) {
+    app.get("/", function (req, res) {
+        res.sendFile(path.join(__dirname, "../public/home.html"));
+    });
+    
+    app.get("/survey", function (req, res) {
+        res.sendFile(path.join(__dirname, "../public/survey.html"));
+    });
+}
 
-// Sets up the Express app to handle data parsing
-app.use(express.urlencoded({ extended: true }));
-app.use(express.json());
-app.use("/css", express.static(__dirname + 'app/'));
-
-app.get("/", function (req, res) {
-    res.sendFile(path.join(__dirname, "../public/home.html"));
-});
-
-app.get("/survey", function (req, res) {
-    res.sendFile(path.join(__dirname, "../public/survey.html"));
-});
-
-app.listen(PORT, function () {
-    console.log("App listening on PORT " + PORT);
-});
